513d7cad8b23a81c5e9a8edeb5ad8e8be383a837
Hardil Desai <hardil.bd@gmail.com>
2025-10-04 18:52:11 +0000

    /map-view has no map view please fix it i am able to see on my dashboard

--- src/app/map-view/page.tsx
+++ src/app/map-view/page.tsx
@@ -1,15 +1,14 @@
-
 'use client';
 
-import { PageWrapper } from '@/components/layout/page-wrapper';
+import { MapLayout } from '@/components/layout/map-layout';
 import { Map } from '@/components/map';
 
 export default function MapViewPage() {
   return (
-    <PageWrapper>
-      <div className="h-full w-full rounded-2xl overflow-hidden">
+    <MapLayout>
+      <div className="h-full w-full">
         <Map showFilters={true} />
       </div>
-    </PageWrapper>
+    </MapLayout>
   );
 }
